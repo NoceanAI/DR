386c3c87dbe18c14d211e9f0cd08eaa19f446ab1
NoceanAI <noceanai@gmail.com>
2020-06-29 09:53:13 -0400

    detection

--- RNET.py
+++ RNET.py
@@ -8,7 +8,7 @@ from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import Dense
 from tensorflow.keras import backend as K 
 
-# RNET 16
+
 class RNET:
 	@staticmethod
 	def build(width, height, depth, classes):
@@ -27,32 +27,27 @@ class RNET:
 			chanDim = 1
 
 
-		model.add(Conv2D(64, (3,3), padding = "same", input_shape = inputShape))
-		model.add(Activation("relu"))
-		model.add(Conv2D(64, (3, 3), padding="same"))
-		model.add(Activation("relu"))
-		model.add(MaxPooling2D(pool_size=(2, 2), strides = (2,2)))
-		model.add(Conv2D(128, (3, 3), padding="same"))
-		model.add(Activation("relu"))
-		model.add(Conv2D(128, (3, 3), padding="same"))
-		model.add(Activation("relu"))
-		model.add(MaxPooling2D(pool_size=(2, 2), strides = (2,2)))
-		model.add(Conv2D(256, (3, 3), padding="same"))
-		model.add(Activation("relu"))
-		model.add(MaxPooling2D(pool_size=(2, 2), strides = (2,2)))
-		model.add(Conv2D(512, (3, 3), padding="same"))
-		model.add(Activation("relu"))
-		model.add(Conv2D(512, (3, 3), padding="same"))
-		model.add(Activation("relu"))
-		model.add(Conv2D(512, (3, 3), padding="same"))
-		model.add(Activation("relu"))
-		model.add(MaxPooling2D(pool_size=(2, 2), strides = (2,2)))
+		model.add(Conv2D(32, (3,3), padding = "same", activation = "relu", input_shape = inputShape))
+		model.add(SeparableConv2D(32, (3, 3), activation='relu'))
+		model.add(Conv2D(32, (3,3), activation = 'relu'))
+		model.add(MaxPooling2D(pool_size=(2, 2)))
+
+		model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+		model.add(SeparableConv2D(64, (3, 3), activation='relu'))
+		model.add(MaxPooling2D(pool_size=(2, 2)))
+
+		model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+		model.add(SeparableConv2D(128, (3, 3), activation='relu'))
+		model.add(MaxPooling2D(pool_size=(2, 2)))
+		
+
+		
+
+
 		model.add(Flatten())
-		model.add(Dense(1024))
-		model.add(Activation("relu"))
+		model.add(Dense(1024, activation='relu'))
 		model.add(Dropout(0.5))
-		model.add(Dense(classes))
-		model.add(Activation("softmax"))
+		model.add(Dense(classes, activation='softmax'))
 
 		# return the model we have built
 		return model
--- RNET_ON_RETINO.py
+++ RNET_ON_RETINO.py
@@ -11,17 +11,15 @@ from refine.preprocessing import simplepreprocessor
 from refine.preprocessing import imagetoarraypreprocessor
 from refine.datasets import simpledatasetloader
 from RNET import RNET
-from tensorflow.keras.optimizers import SGD
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import SGD, Adam
 from sklearn.model_selection import train_test_split
 from imutils import paths
 
-
+lr_init = 0.001
 
 ap = argparse.ArgumentParser()
 #ap.add_argument("-o", "--output", required = True, help = "path to the output pllot")
 ap.add_argument("-d", "--dataset", required=True, help="path to input dataset")
-ap.add_argument("-a", "--augment", type=int, default=-1, help="whether or not 'on the fly' data augmentation should be used")
 ap.add_argument("-m", "--model", required=True,
 help="path to output model")
 args = vars(ap.parse_args())
@@ -34,7 +32,7 @@ imagePaths = list(paths.list_images(args["dataset"]))
 #idxs = np.random.randint(0, len(imagePaths), size=(10,))
 #imagePaths = imagePaths[idxs]
 
-sp = simplepreprocessor.SimplePreprocessor(64, 64)
+sp = simplepreprocessor.SimplePreprocessor(32, 32)
 iap = imagetoarraypreprocessor.ImageToArrayPreprocessor()
 
 sdl = simpledatasetloader.SimpleDatasetLoader([sp, iap])
@@ -50,30 +48,15 @@ trainY = LabelBinarizer().fit_transform(trainY)
 testY = LabelBinarizer().fit_transform(testY)
 #print(testX.shape)
 
-aug = ImageDataGenerator()
-
-if args["augment"] > 0:
-	print("performing 'on the fly' data augmentation")
-	aug = ImageDataGenerator(
-		rotation_range=20,
-		zoom_range=0.15,
-		width_shift_range=0.2,
-		height_shift_range=0.2,
-		shear_range=0.15,
-		horizontal_flip=True,
-		fill_mode="nearest")
-
 print("compiling................ Please Wait...")
-opt = SGD(lr = 0.005, momentum = 0.9, decay = 0.005/100)
-model = RNET.build(width = 64, height = 64, depth = 3, classes = 5)
+
+opt = Adam(lr = lr_init, decay = lr_init/100)
+model = RNET.build(width = 32, height = 32, depth = 3, classes = 5)
 model.compile(loss = "categorical_crossentropy", optimizer = opt, metrics = ["accuracy"])
 print(model.summary())
 
 print("[CAUTION] Training about to occur, do not interrupt")
-H = model.fit(
-	x = aug.flow(trainX, trainY, batch_size=32), 
-	validation_data=(testX, testY), 
-	epochs=100, verbose=1)
+H = model.fit(trainX, trainY, validation_data=(testX, testY), batch_size=32, epochs=100, verbose=1)
 
 print("Serializing Network")
 model.save(args["model"])
